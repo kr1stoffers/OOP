0495e7d7fb0b53f2b66ae52394f42dc49beb35b1
Kr1stoffers <dan-ivaya@mail.ru>
2023-12-15 14:29:24 +0500

    l4-5

--- lab_4/main.cpp
+++ lab_4/main.cpp
@@ -16,28 +16,78 @@ T minmin(T a, T b, T c) {
 template <typename T>
 class Arr {
    private:
-   public:
-    Arr() {}
-};
-
-template <class T>
-class myklass {
-    T *data;
+    T *array;
     int size;
-    int n;
 
    public:
-    myklass(int);
-    T &operator[](int i) { return data[i]; }
-};
-template <class T>
-myklass<T>::myklass(int) {
-    data = new T(n);
-    size = n;
+    Arr(int size) {
+        this->size = size;
+        array = new T[size];
+    }
+
+    void fill() {
+        cout << "\nEnter " << size << " elements:" << endl;
+        for (int i = 0; i < size; i++) {
+            cout << i + 1 << ". ";
+            cin >> array[i];
+        }
+    }
+
+    void sort() {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (array[i] > array[j]) {
+                    T tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+    }
+
+    T getMin() {
+        T min = array[0];
+        for (int i = 1; i < size; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+        }
+        return min;
+    }
+
+    T getMax() {
+        T max = array[0];
+        for (int i = 1; i < size; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+    }
+
+    void print() {
+        for (int i = 0; i < size; i++) {
+            cout << array[i] << " ";
+        }
+        cout << endl;
+    }
 };
 
 int main(int argc, char const *argv[]) {
     cout << "int: " << minmin(3, 2, 1) << endl;
     cout << "float: " << minmin(1.1, 1.2, 1.3) << endl;
     cout << "char: " << minmin('a', 'b', 'c');
+
+    Arr<int> array(7);
+    array.fill();
+    cout << "unsorted: " << endl;
+    array.print();
+    cout << "sorted: " << endl;
+    array.sort();
+    array.print();
+
+    cout << "max: " << array.getMax() << endl;
+    cout << "min: " << array.getMin() << endl;
+
+    getchar();
 }
